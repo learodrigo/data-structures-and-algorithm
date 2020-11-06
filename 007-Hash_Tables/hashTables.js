571842a5677b9ed2a7a6e8076fa08ca1d59515dd
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-11-07 00:47:38 +0100

    Hash tables

---
+++ 007-Hash_Tables/hashTables.js
@@ -0,0 +1,75 @@
+class HashTable {
+    constructor () {
+        this.storage = []
+        this.storageLimit = 100
+    }
+
+    hash (string, max) {
+        let hash = 0
+        string.split('').map((_, i) => hash += string.charCodeAt(i))
+        return hash % max
+    }
+
+    print () {
+        console.log(this.storage)
+    }
+
+    add (key, value) {
+        const index = this.hash(key, this.storageLimit)
+
+        if (!this.storage[index]) {
+            this.storage[index] = [ [key, value] ]
+        }
+        else {
+            let inserted = false
+
+            for (let i = 0; i < this.storage[index].length; i++) {
+                if (this.storage[index][i][0] === key) {
+                    this.storage[index][i][1] = value
+                    inserted = true
+                }
+            }
+
+            if (!inserted) this.storage[index].push([key, value])
+        }
+    }
+
+    remove (key) {
+        const index = this.hash(key, this.storageLimit)
+
+        if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+            delete this.storage[index]
+        }
+        else {
+            for (let i = 0; i < this.storage[index].length; i++) {
+                if (this.storage[index][i][0] === key) {
+                    delete this.storage[index][i]
+                }
+            }
+        }
+    }
+
+    lookup (key) {
+        const index = this.hash(key, this.storageLimit)
+
+        if (!this.storage[index]) {
+            return undefined
+        }
+        else {
+            for (let i = 0; i < this.storage[index].length; i++) {
+                if (this.storage[index][i][0] === key) {
+                    return this.storage[index][i][1]
+                }
+            }
+        }
+    }
+}
+
+const ht = new HashTable()
+ht.add('beau', 'person')
+ht.add('fido', 'dog')
+ht.add('rex', 'dinosour')
+ht.add('tux', 'penguin')
+
+console.log('lookup(tux):', ht.lookup('tux'))
+ht.print()
